bef683d6be8bf931517965efe34b9b504f9b7806
anniehedgpeth <annie.hedgpeth@gmail.com>
2017-03-17 15:10:04 +0000

    added erb

--- recipes/server.rb
+++ recipes/server.rb
@@ -9,12 +9,8 @@ powershell_script 'Install IIS' do
   action :run
 end
 
-file 'C:\inetpub\wwwroot\Default.htm' do
-  content "<h1>Hello, world!</h1>
-  <h2>ipaddress: #{node['ipadress']}</h2>
-  <h2>hostname: #{node['hostname']}</h2>
-  <h2>total memory: #{node['memory']['total']}</h2>
-  <h2>CPU Mhz: #{node['cpu']['0']['mhz']}</h2>"
+template 'C:\inetpub\wwwroot\Default.htm' do
+  source 'Default.htm.erb'
 end
 
 service 'w3svc' do
